d79ccd23bfc726379545a137503c6ffe7775085e
Gonz Tirado <tamudo84@gmail.com>
2018-04-05 20:11:48 +0200

    - Añadido script para controlar el giro de la camara

---
+++ Assets/Scripts/PlayerMoveController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerMoveController : MonoBehaviour
+{
+    [Header("Move")] public string horizontalAxis;
+    public string verticalAxis;
+    public float moveSpeed;
+
+    void Update()
+    {
+        CheckMove();
+    }
+
+    private void CheckMove()
+    {
+        float h = Input.GetAxisRaw(horizontalAxis);
+        float v = Input.GetAxisRaw(verticalAxis);
+
+        if (h != 0 || v != 0)
+        {
+            transform.Rotate(Vector3.up * h  * moveSpeed * Time.deltaTime);
+        }
+    }
+}
