64ebe746446f5cbea9aed688675fd14cd3310757
Gonz Tirado <tamudo84@gmail.com>
2018-04-06 10:45:18 +0200

    - Refactorizado nombre de variables del player controller para que quede más claro

--- Assets/Scripts/Player/PlayerMoveController.cs
+++ Assets/Scripts/Player/PlayerMoveController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerMoveController : MonoBehaviour
 {
     [Header("Mouse Raycast Settings")]
-    public LayerMask floorMask; // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
-    public float camRayLength = 100f; // The length of the ray from the camera into the scene.
+    public LayerMask mouseHitMask; // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
+    public float mouseRayLength = 100f; // The length of the ray from the camera into the scene.
 
     void Update()
     {
@@ -19,7 +19,7 @@ public class PlayerMoveController : MonoBehaviour
         Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit floorHit;
 
-        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        if (Physics.Raycast(camRay, out floorHit, mouseRayLength, mouseHitMask))
         {
             // Create a vector from the player to the point on the floor the raycast from the mouse hit.
             // Ensure the vector is entirely along the floor plane.
