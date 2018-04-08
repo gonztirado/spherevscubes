a6a6f83575d52fb6051ec585e71a1156a8c0895e
Gonz Tirado <tamudo84@gmail.com>
2018-04-08 22:28:11 +0200

    Añadido control de la barra de tiempo

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -12,14 +12,18 @@ public class GameController : MonoBehaviour
     public Text ammoPanel;
     public Image timeBar;
     public Text levelName;
-    public Text killsCounter;
+    public Text killsCounterText;
 
     [Header("Game elements")] public GameObject player;
     public GameObject enemies;
     public GameObject bullets;
     public GameObject cameras;
 
+    [Header("Game settings")] public float levelTimeInSeconds;
+
     private bool _isGameStarted;
+    private int _killsCounter;
+    private float _levelTimeRemaining;
 
     void Awake()
     {
@@ -39,7 +43,18 @@ public class GameController : MonoBehaviour
 
     void Update()
     {
+        UpdateLevelRemainingTime();
     }
+
+    private void UpdateLevelRemainingTime()
+    {
+        if (_isGameStarted && Time.timeScale > 0)
+        {
+            _levelTimeRemaining -= Time.deltaTime;
+            UpdateTimeBar();
+        }
+    }
+
 
     public bool IsGameStarted
     {
@@ -49,20 +64,34 @@ public class GameController : MonoBehaviour
     public void ResetGame()
     {
         _isGameStarted = true;
+        ResetHubElements();
         ResetPlayerProperties();
-        ResetStage();
+        DeleteStageElements();
+    }
+
+    private void ResetHubElements()
+    {
+        player.GetComponent<Health>().RecoverAllHealth();
+        _killsCounter = 0;
+        killsCounterText.text = _killsCounter.ToString();
+        _levelTimeRemaining = levelTimeInSeconds;
+        UpdateTimeBar();
     }
 
     private void ResetPlayerProperties()
     {
         cameras.transform.rotation = Quaternion.identity;
         player.transform.rotation = Quaternion.identity;
-        
     }
 
-    private void ResetStage()
+    private void DeleteStageElements()
     {
         GeneralUtils.DestroyAllChildren(enemies.transform);
         GeneralUtils.DestroyAllChildren(bullets.transform);
+    }
+
+    private void UpdateTimeBar()
+    {
+        timeBar.fillAmount = _levelTimeRemaining / levelTimeInSeconds;
     }
 }
