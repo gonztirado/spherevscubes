6a1f91a1c36f88c1dd894643f2097864f5c49e6d
Gonz Tirado <tamudo84@gmail.com>
2018-04-15 23:12:37 +0200

    Añadido manager para activar/desactivar las IAs con teclas

--- Assets/Scripts/IA/IAManager.cs
+++ Assets/Scripts/IA/IAManager.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class IAManager : MonoBehaviour
 {
+    public static IAManager instance;
 
-	public static IAManager instance;
+    [Header("IA Controllers")] public IACameraController iaCameraController;
+    public IAPlayerMoveController iaPlayerMoveController;
+    public IAGunController iaGunController;
 
-	void Awake()
-	{
-		if (instance == null)
-			instance = this;
-		else if (instance != this)
-			Destroy(gameObject);
-		DontDestroyOnLoad(gameObject);
-	}
+    [Header("IA active Keys")] public KeyCode IaActiveAllKey;
+    public KeyCode IaCameraControllerKey;
+    public KeyCode IaPlayerMoveControllerKey;
+    public KeyCode IaGunControllerKey;
+
+    private bool _isActiveAllIA;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(IaActiveAllKey))
+            ToogleAllIA();
+        if (Input.GetKeyUp(IaCameraControllerKey))
+            ToogleIACamera();
+        if (Input.GetKeyUp(IaPlayerMoveControllerKey))
+            ToogleIAPlayerMove();
+        if (Input.GetKeyUp(IaGunControllerKey))
+            ToogleIAGun();
+    }
+
+    private void ToogleAllIA()
+    {
+        _isActiveAllIA = !_isActiveAllIA;
+        iaCameraController.enableIA = _isActiveAllIA;
+        iaPlayerMoveController.enableIA = _isActiveAllIA;
+        iaGunController.enableIA = _isActiveAllIA;
+        GameController.instance.UpdateGameStatusText("ALL AI " + (_isActiveAllIA ? "ENABLED" : "DISABLED"), 1.5f);
+    }
+
+    private void ToogleIACamera()
+    {
+        iaCameraController.enableIA = !iaCameraController.enableIA;
+        GameController.instance.UpdateGameStatusText("CAMERA AI " + (iaCameraController.enableIA ? "ENABLED" : "DISABLED"), 1.5f);
+    }
+
+    private void ToogleIAPlayerMove()
+    {
+        iaPlayerMoveController.enableIA = !iaPlayerMoveController.enableIA;
+        GameController.instance.UpdateGameStatusText("PLAYER MOVE AI " + (iaPlayerMoveController.enableIA ? "ENABLED" : "DISABLED"), 1.5f);
+    }
+
+    private void ToogleIAGun()
+    {
+        iaGunController.enableIA = !iaGunController.enableIA;
+        GameController.instance.UpdateGameStatusText("GUN AI " + (iaGunController.enableIA  ? "ENABLED" : "DISABLED"), 1.5f);
+    }
 }
