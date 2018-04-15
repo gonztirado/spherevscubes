afb4991c38c703c6457e4c847dbf5b045c2356c6
Gonz Tirado <tamudo84@gmail.com>
2018-04-15 12:35:23 +0200

    Añadidas zonas de detección de enemigos. Están en el objeto de la camara porque se moverán con ellas, hay una central en la vista de la camara, otras dos a izquierda y derecha y otra en la espalda para detectar lo que se ve desde el retrovisor

---
+++ Assets/Scripts/IA/Camera/IAEnemyDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAEnemyDetector : MonoBehaviour
+{
+    [Header("Debug")] public bool drawGizmos;
+
+
+    [Header("IA Settings")] public LayerMask layerEnemies;
+    public float detectorRadius;
+
+
+    private void OnDrawGizmos()
+    {
+        if (drawGizmos)
+        {
+            Gizmos.DrawWireSphere(transform.position, detectorRadius);
+        }
+    }
+}
